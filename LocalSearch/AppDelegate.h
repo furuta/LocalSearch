e55a7586fe984c5ce87ee1acd2849f2f1028ad11
furuta <furuta.tom.8@gmail.com>
2014-04-12 21:30:04 +0900

    Initial Commit

---
+++ LocalSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocalSearch
+//
+//  Created by アンビシェイト on 2014/04/12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocalSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocalSearch
+//
+//  Created by アンビシェイト on 2014/04/12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
